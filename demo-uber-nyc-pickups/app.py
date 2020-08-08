30bb6afe9dd8050acd8c90483c8be4daf5352e68
Eliud Kagema <eliud.k.maina@gmail.com>
2020-08-08 22:15:58 +0300

    playing with st

---
+++ demo-uber-nyc-pickups/app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+import pydeck as pdk
+
+
+# hide hambuger menu
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
+
+
+DATE_TIME = "date/time"
+DATA_URL = (
+    "http://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz"
+)
+
+st.title("Uber Pickups in New York City")
+st.markdown(
+"""
+This is a demo of a Streamlit app that shows the Uber pickups
+geographical distribution in New York City. Use the slider
+to pick a specific hour and look at how the charts change.
+""")
+
+@st.cache(persist=True)
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis="columns", inplace=True)
+    data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
+    return data
+
+data = load_data(10000)
+
+hour= st.sidebar.slider('hour', 0, 23, 10)
+data = data[data[DATE_TIME].dt.hour == hour]
+
+'# Geo Data at %sh' % hour
+st.map(data)
+
+'data', data
